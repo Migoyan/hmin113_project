90b6b85f84b74f0923f575fc13ceaf2b90edc59c
arthur <64397268+Migoyan@users.noreply.github.com>
2020-11-11 17:34:48 +0100

    Gene_CI.py est maintenant fonctionnel et gère les masses min et max

--- Gene_CI.py
+++ Gene_CI.py
@@ -10,23 +10,27 @@ Listes paramètres
 ###                      Packages
 
 import sys
+import random as r
 
 ###                      Functions
 
 
 ###                      Main program
 
-# Vérification de la bonne valeur du paramètre donné, N_objet étant le nombre d'objet
-try :
-    N_objet =  int(sys.argv[1])  #ValueError
-    if N_objet < 0 :
-        raise ValueError
-    else :
-        print("Paramètre correct, valeur : ", N_objet, "\n" +
-              "Execution du code")
-except ValueError:
-    print("Il faut un nombre entier positif, relancer le programme")
+# Paramètres données
 
+N_objet =  int(sys.argv[1])
+m_min = int(sys.argv[2])
+m_max = int(sys.argv[3])
 
+corps = []   # liste de liste, un corps = [x, y, Vx, Vy, m]
+
+for i in range(N_objet):
+    corps += [[r.uniform(0,100),r.uniform(0,100),r.uniform(0,5),r.uniform(0,5),r.uniform(m_min, m_max)]]
 
 # Ecriture des données dans un fichier
+
+fichier = open('CI.dat', 'w')
+for i in corps:
+    fichier.write(str(i)+"\n")
+fichier.close()
