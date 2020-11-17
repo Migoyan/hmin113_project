33ee5fd0b1ca14df07c02f2245bf6f626125454c
arthur <64397268+Migoyan@users.noreply.github.com>
2020-11-17 13:13:49 +0100

    Fonction d'init enlevée et corrections

--- integration.py
+++ integration.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from math import *
-from matplotlib import pyplot as plt
+import math as m
+import matplotlib.pyplot as plt
 
 
-g=0.00001
+# Constantes
+g=1      # Constante gravitationnelle
 
-h=0.00001
+h=0.00001         # Pas de temps
 
 
 Obj=[]
@@ -14,39 +15,25 @@ Obj=[]
 
 #structure des objets [x0,y0,vx0,vy0,m]
 
-Obj+=[[1,0,0,0,1]]
-Obj+=[[0,0,0,0,100000]]
+Obj+=[[[1000],[0],[0],[10],1]]
+Obj+=[[[0],[0],[0],[0],10000]]
 
-#calcule des acceleration
+#calcul des accélérations
 def a_x(x1,y1,x2,y2,m1,m2):
 	if (x1-x2)!=0:
-		return -g*m1*m2*(1./((x1-x2)**2+(y1-y2)**2))*((x1-x2)/sqrt((x1-x2)**2+(y1-y2)**2))
+		return g*m1*m2*(1./((x1-x2)**2+(y1-y2)**2))*((x2-x1)/m.sqrt((x1-x2)**2+(y1-y2)**2))
 	else:
 		return 0
 
 
 def a_y(x1,y1,x2,y2,m1,m2):
 	if (y1-y2)!=0:
-		return -g*m1*m2*(1./((x1-x2)**2+(y1-y2)**2))*((y1-y2)/sqrt((x1-x2)**2+(y1-y2)**2))
+		return g*m1*m2*(1./((x1-x2)**2+(y1-y2)**2))*((y2-y1)/m.sqrt((x1-x2)**2+(y1-y2)**2))
 	else:
 		return 0
 
 
-#remplace les valeur de x,y et v par des list
-def init_obj(L):
-	for i in range(len(L)):
-		x=L[i][0]
-		y=L[i][1]
-		vx=L[i][2]
-		vy=L[i][3]
-		L[i][0]=[x]
-		L[i][1]=[y]
-		L[i][2]=[vx]
-		L[i][3]=[vx]
-	return(L)
-
-
-#methode de runge-kutta à l'ordre 4
+#méthode de runge-kutta à l'ordre 4
 def k_ax(x1,y1,x2,y2,m1,m2,f):
 	k=f(x1,y1,x2,y2,m1,m2)
 	k2=f(x1+h*k/2,y1,x2,y2,m1,m2)
@@ -62,46 +49,46 @@ def k_ay(x1,y1,x2,y2,m1,m2,f):
 	return(k+2*k2+2*k3+k4)/6
 
 
-#fonction pour integre 
+#fonction pour integré
 
 def integration(L):
-	obj=init_obj(L)
+	obj=L
 	for i in range(100000):
-		#liste qui contiene les acceleration au pas i
+		#liste qui contient les accelerations au pas i
 		ax=[]
 		ay=[]
 		Lobj=obj
 
-		#calcule des accelerations un seul calcule par couple d'objet
+		#calcul des accelerations un seul calcul par couple d'objet
 		for obj1 in Lobj[:-1]:
-			Lobj=Lobj[1:] #j'enleve l'élement de la liste pour ne pas recalculer les accéleration  associer
+			Lobj=Lobj[1:] #j'enlève l'élément de la liste pour ne pas recalculer les accélerations  associées
 			
-			#initialisation de liste pour contenir les accéleration de façon temporaire
+			#initialisation de liste pour contenir les accélérations de façon temporaire
 			Lax=[]
 			Lay=[]
-			#2 eme boucle pour selectionner les objet restant 
+			#2 ième boucle pour sélectionner les objets restants 
 			for obj2 in Lobj:
 				Lax+=[k_ax(obj1[0][i],obj1[1][i],obj2[0][i],obj2[1][i],obj1[4],obj2[4],a_x)] 
 				Lay+=[k_ay(obj1[0][i],obj1[1][i],obj2[0][i],obj2[1][i],obj1[4],obj2[4],a_y)]
 			ax+=[Lax]
 			ay+=[Lay]
 
-		#ajout de l'element i+1 pour x,y et v , initialiser à 0
+		#ajout de l'élément i+1 pour x, y et v, initialiser à 0
 		for obj1 in obj:
 			for obj2 in obj1[:-1]:
 				obj2+=[0]
 
-		#calcule des vitesse
+		#calcul des vitesses
 		for j in range(len(ax)):
 			for k in range(len(ax[j])):
-				#ajout des acceleration sur l'objet j
+				#ajout des accélérations sur l'objet j
 				obj[j][2][i+1]+=obj[j][2][i]+h*ax[j][k]/obj[j][4]
 				obj[j][3][i+1]+=obj[j][3][i]+h*ay[j][k]/obj[j][4]
-				#ajout de l'acceleration sur l'objet qui luis asocier pour l'accelaration
+				#ajout de l'accélération sur l'objet qui luis asocier pour l'accélération
 				obj[j+k+1][2][i+1]+=obj[j+k+1][2][i]-h*ax[j][k]/obj[j+k+1][4]
 				obj[j+k+1][3][i+1]+=obj[j+k+1][3][i]-h*ay[j][k]/obj[j+k+1][4]
 		
-		#calcule des position
+		#calcul des positions
 		for objet in obj:
 			objet[0][i+1]+=objet[0][i]+h*objet[2][i]
 			objet[1][i+1]+=objet[1][i]+h*objet[3][i]
