378c5e08bf41224a661069b7aaee80916b3af7dd
arthur <64397268+Migoyan@users.noreply.github.com>
2020-11-17 13:15:29 +0100

    La position, vitesse min et max sont maintenant définis par l'utilisateur

--- Gene_CI.py
+++ Gene_CI.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+*#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 """
@@ -11,6 +11,7 @@ Listes paramètres
 
 import sys
 import random as r
+import math as m
 
 ###                      Functions
 
@@ -19,14 +20,27 @@ import random as r
 
 # Paramètres données
 
-N_objet =  int(sys.argv[1])
-m_min = int(sys.argv[2])
-m_max = int(sys.argv[3])
+N_objet = int(sys.argv[1])
+x_min = float(sys.argv[2])
+x_max = float(sys.argv[3])
+y_min = float(sys.argv[4])
+y_max = float(sys.argv[5])
+v_min = float(sys.argv[6])
+v_max = float(sys.argv[7])
+m_min = float(sys.argv[8])
+m_max = float(sys.argv[9])
 
 corps = []   # liste de liste, un corps = [x, y, Vx, Vy, m]
 
 for i in range(N_objet):
-    corps += [[r.uniform(0,100),r.uniform(0,100),r.uniform(0,5),r.uniform(0,5),r.uniform(m_min, m_max)]]
+    direction = r.uniform(0, 2*m.pi)
+    direction_x = m.cos(direction)
+    direction_y = m.sin(direction)
+    corps += [[[r.uniform(x_min, x_max)],
+               [r.uniform(y_min, y_max)],
+               [r.uniform(v_min * direction_x, v_max * direction_x)],
+               [r.uniform(v_min * direction_y, v_max * direction_y)],
+               r.uniform(m_min, m_max)]]
 
 # Ecriture des données dans un fichier
 
