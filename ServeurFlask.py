7c70072bbd6a039a59bf8e329145ea5012258ccf
arthur <64397268+Migoyan@users.noreply.github.com>
2020-11-17 13:16:24 +0100

    Compatibilité serveur flask et fichier Gene_CI

--- ServeurFlask.py
+++ ServeurFlask.py
@@ -17,20 +17,30 @@ def index():
     return "Bienvenue sur ce serveur Flask local avec lequel vous pouvez communiquer\
  pour faire des simulations du problème à n corps"
 
-@serveur.route('/paramInit/<int:nbObj>/<int:mMin>/<int:mMax>')
+@serveur.route('/paramInitAleat/<int:nbObj>/<xMin>/<xMax>/<yMin>/<yMax>/<vMin>/<vMax>/<float:mMin>/<float:mMax>')
 
-def paramInit(nbObj, mMin, mMax):
+def paramInitAleat(nbObj, xMin, xMax, yMin, yMax, vMin, vMax, mMin, mMax):
     """
-    Cette fonction reçoit les paramètres pour la génération de conditions initiales
+    Cette fonction reçoit les paramètres pour la génération de conditions initiales aléatoires
     """
-    bashCommand = './Gene_CI.py {} {} {}'.format(nbObj, mMin, mMax)
+    bashCommand = './Gene_CI.py {} {} {} {} {} {} {} {} {}'.format(nbObj,
+                                                                   xMin, xMax, yMin, yMax
+                                                                   vMin, vMax,
+                                                                   mMin, mMax)
     process = sb.Popen(bashCommand.split(), stdout = sb.PIPE)
     output, error = process.communicate()
     if error == None:
         return "Commande executée sans problème"
     else:
         return "Erreur dans l'execution interne, \nLog d'erreur : " + str(error)
-    
+
+@serveur.route('/paramInitList/<corps>')
+
+def paramInitList(corps):
+    """
+    Cette fonction reçoit une liste d'objets initiaux pour la simulation
+    """
+    return
 
 @serveur.route('/retourDonnees/<result>')
 
