caf2f66f17706b2eb1a82c349d291a92299adc7d
arthur <64397268+Migoyan@users.noreply.github.com>
2020-11-18 17:12:58 +0100

    Update ServeurFlask : Possibilité d'upload un fichier

--- ServeurFlask.py
+++ ServeurFlask.py
@@ -5,8 +5,10 @@ import flask as flk
 import sys
 import subprocess as sb
 
+UPLOAD_FOLDER = '/'
 
 serveur = flk.Flask(__name__)
+serveur.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @serveur.route('/')  # route : localhost:5000/
 
@@ -17,14 +19,14 @@ def index():
     return "Bienvenue sur ce serveur Flask local avec lequel vous pouvez communiquer\
  pour faire des simulations du problème à n corps"
 
-@serveur.route('/paramInitAleat/<int:nbObj>/<xMin>/<xMax>/<yMin>/<yMax>/<vMin>/<vMax>/<float:mMin>/<float:mMax>')
+@serveur.route('/paramInit/<int:nbObj>/<float:xMin>/<float:xMax>/<float:yMin>/<float:yMax>/<float:vMin>/<float:vMax>/<float:mMin>/<float:mMax>')
 
-def paramInitAleat(nbObj, xMin, xMax, yMin, yMax, vMin, vMax, mMin, mMax):
+def param_init(nbObj, xMin, xMax, yMin, yMax, vMin, vMax, mMin, mMax):
     """
     Cette fonction reçoit les paramètres pour la génération de conditions initiales aléatoires
     """
     bashCommand = './Gene_CI.py {} {} {} {} {} {} {} {} {}'.format(nbObj,
-                                                                   xMin, xMax, yMin, yMax
+                                                                   xMin, xMax, yMin, yMax,
                                                                    vMin, vMax,
                                                                    mMin, mMax)
     process = sb.Popen(bashCommand.split(), stdout = sb.PIPE)
@@ -34,17 +36,21 @@ def paramInitAleat(nbObj, xMin, xMax, yMin, yMax, vMin, vMax, mMin, mMax):
     else:
         return "Erreur dans l'execution interne, \nLog d'erreur : " + str(error)
 
-@serveur.route('/paramInitList/<corps>')
+@serveur.route('/uploadCIFile', methods = ['POST'])
 
-def paramInitList(corps):
+def upload_CI_file():
     """
-    Cette fonction reçoit une liste d'objets initiaux pour la simulation
+    Cette fonction permet de recevoir un fichier de l'utilisateur
+    Pour uploadé un fichier (commande bash) :
+    curl -X POST -F filename=@Nom_fichier http://localhost:5000/uploadCIFile
     """
-    return
+    f = flk.request.files['filename']
+    f.save("CI.dat")
+    return "fichier uploadé"
 
 @serveur.route('/retourDonnees/<result>')
 
-def retourDonnees(result):
+def retour_donnees(result):
     try:
         return flk.send_file(result)
     except FileNotFoundError:
